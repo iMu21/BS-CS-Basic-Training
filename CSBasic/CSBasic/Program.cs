22563e3661ec579d2d385a0228f41684997197f7
iMu21 <ik21.cse14@gmail.com>
2022-08-11 11:59:07 +0600

    overflow exception handeled using checked

--- CSBasic/CSBasic/Program.cs
+++ CSBasic/CSBasic/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int maxInteger = int.MaxValue;
+int number1 = 30;
+int number2 = 1234;
+
+try
+{
+    checked
+    {
+        var number = number1 + maxInteger;
+    }
+}
+catch (OverflowException e)
+{
+    Console.WriteLine($"{number1} + {maxInteger} " + e.Message);
+}
+
+try
+{
+    checked
+    {
+        var number = number1 + number2;
+        Console.WriteLine($"{number1} + {number2} = {number}");
+    }
+}
+catch (OverflowException e)
+{
+    Console.WriteLine(e.Message);
+}
