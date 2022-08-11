8ea7de5bbb7c006af72827cbc5e08ee0fe29209d
iMu21 <ik21.cse14@gmail.com>
2022-08-11 11:56:26 +0600

    primitive data types used and printed values

--- CSBasic/CSBasic/Program.cs
+++ CSBasic/CSBasic/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string a_string = "'I am a string.'";
+Console.WriteLine($"Example of a string = {a_string}");
+
+bool a_bool = true;
+Console.WriteLine($"Example of a bool = {a_bool}");
+
+byte a_byte = 24; //byte is worked for unsigned 8 bit data (0 to 255)
+Console.WriteLine($"Example of a byte = {a_byte}");
+
+sbyte a_sbyte = -50; //sbyte is worked for signed 8 bit data (-127 to 128)
+Console.WriteLine($"Example of a sbyte = {a_sbyte}");
+
+short a_short = -31002; //Signed 16-bit integer (-32768 to 32767)
+Console.WriteLine($"Example of a short = {a_short}");
+
+int an_int = 21; //signed 32-bit integer (-2147483648 to 2147483647)
+Console.WriteLine($"Example of a int = {an_int}");
+
+uint an_uint = 444; //unsigned 32-bit integer (0 to 4294967295)
+Console.WriteLine($"Example of a unit = {an_uint}");
+
+long a_long = -23423455; //signed 64-bit integer (-9223372036854775808 to 9223372036854775807)
+Console.WriteLine($"Example of a long = {a_long}");
+
+ulong an_ulong = 3376736723767; //unsigned 64-bit integer (0 to 18446744073709551615)
+Console.WriteLine($"Example of a ulong = {an_ulong}");
+
+char a_char = 'd';
+Console.WriteLine($"Example of a char = {a_char}");
+
+double a_double = 34.56;
+Console.WriteLine($"Example of a double = {a_double}");
+
