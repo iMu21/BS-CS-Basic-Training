d0cf44094ca6ee99d58bb18860b2d156d821fcc6
iMu21 <ik21.cse14@gmail.com>
2022-08-11 11:54:31 +0600

    if , else, if else examples are used

--- CSBasic/CSBasic/Program.cs
+++ CSBasic/CSBasic/Program.cs
@@ -1,1 +1,41 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter a positive number");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (isPrimeNumber(number))
+{
+    Console.WriteLine($"{number} is a prime number\n");
+}
+else
+{
+    Console.WriteLine($"{number} is not a prime number\n");
+}
+
+
+
+Console.WriteLine("Enter a character");
+char c = Convert.ToChar(value: Console.ReadLine());
+
+if (c >= 'a' && c <= 'z')
+{
+    Console.WriteLine("Small letter\n");
+}
+else if (c >= 'A' && c <= 'Z')
+{
+    Console.WriteLine("Capital letter\n");
+}
+else
+{
+    Console.WriteLine("Not a letter\n");
+}
+
+bool isPrimeNumber(long a)
+{
+    if (a == 1 || a == 2) return true;
+    for (int i = 2; i * i <= a; i++)
+    {
+        if (a % i == 0) return false;
+    }
+    return true;
+
+}
