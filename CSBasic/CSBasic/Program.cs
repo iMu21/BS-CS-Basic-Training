859fd12516c765c7bd6cf62716f3009898d555f4
iMu21 <ik21.cse14@gmail.com>
2022-08-11 12:00:59 +0600

    ternary operations used

--- CSBasic/CSBasic/Program.cs
+++ CSBasic/CSBasic/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Write down the total mark of an examination to see grade (0 to 100");
+
+long mark = Convert.ToInt64(Console.ReadLine());
+
+var result = mark < 30 ? "F" : mark < 40 ? "D" : mark < 50 ? "C" : mark < 60 ? "B" : mark < 70 ? "A-" : mark < 80 ? "A" : "A+";
+
+
+Console.WriteLine($"Occupied grade for {mark} is {result}");
