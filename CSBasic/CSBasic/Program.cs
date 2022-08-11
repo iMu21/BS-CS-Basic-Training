4be1f400d7293309560a6c7a56f9777331c561c2
iMu21 <ik21.cse14@gmail.com>
2022-08-11 11:49:39 +0600

    Defferent loops and comments are used

--- CSBasic/CSBasic/Program.cs
+++ CSBasic/CSBasic/Program.cs
@@ -1,1 +1,61 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int a = 1;
+
+//using do loop
+do
+{
+    Console.Write(a + " ");
+    a++;
+} while (a < 10);
+Console.WriteLine("\n");
+
+
+
+/* using
+ * while
+ * loop */
+
+while (a != 0)
+{
+    Console.Write(a + " ");
+    a--;
+}
+Console.WriteLine("\n");
+
+//for loop
+for (int i = 0; i < 10; i++)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine("\n");
+
+
+/* foreach loop */
+foreach (char c in "foreach")
+{
+    Console.Write(c + " ");
+}
+Console.WriteLine("\n");
+
+//loo[ing 1 to 99 ,skip at 95 and breaking at 99
+a = 1;
+while (true)
+{
+    if (a == 99)
+    {
+        Console.Write(a + "\n");
+        break;
+    }
+    else if (a == 95)
+    {
+        a++;
+        continue;
+    }
+    else
+    {
+        Console.Write(a + " ");
+    }
+    a++;
+}
+/// <summary>
+/// Task is completed.
+/// </summary>
