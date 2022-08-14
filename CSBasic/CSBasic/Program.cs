47cfb2c4116956e39f88c91d3262f50021f92347
iMu21 <ik21.cse14@gmail.com>
2022-08-14 13:06:11 +0600

    same methodName in different class

--- CSBasic/CSBasic/Program.cs
+++ CSBasic/CSBasic/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello World!");
+﻿class StudentDataAnalysis
+{
+    public void printData()
+    {
+        Console.WriteLine("Data Analysed");
+    }
+}
+
+class StudentDataProcessing
+{
+    public static void printData()
+    {
+        Console.WriteLine("Data Processed");
+    }
+}
+
+class Student
+{
+    public static void Main(string[] args)
+    {
+        StudentDataAnalysis analysis = new StudentDataAnalysis();
+        analysis.printData();
+        StudentDataProcessing.printData();
+    }
+}
