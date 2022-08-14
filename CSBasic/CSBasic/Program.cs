ebad8a693f416f4a17f5ea9cc10768fc434572e2
iMu21 <ik21.cse14@gmail.com>
2022-08-14 12:43:22 +0600

    enum declared and used

--- CSBasic/CSBasic/Program.cs
+++ CSBasic/CSBasic/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello World!");
+﻿
+public enum PlanetRadius
+{
+    Jupiter = 71492,
+    Saturn = 60268,
+    Uranus = 25559,
+    Neptune = 24764,
+    Earth = 6378,
+    Venus = 6052,
+    Mars = 3396,
+    Mercury = 2439,
+    Moon = 1738,
+    Pluto = 1195
+
+}
+class enumDeclaration
+{
+    public static double AreaOfPlanet(PlanetRadius planet)
+    {
+        return Math.PI * (double)((int)planet * (int)planet);
+    }
+    public static void Main(string[] args)
+    {
+
+        PlanetRadius planet = PlanetRadius.Earth;
+        Console.WriteLine($"Area of {planet} is {AreaOfPlanet(planet)} km^2");
+
+    }
+}
